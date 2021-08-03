891434051530bf16c8d1e3c046f278524be83bf8
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-08-03 13:44:58 +0200

    add normalize to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
-import { Provider } from 'react-redux';
-import { GlobalStyle } from './GlobalStyle';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Normalize } from "styled-normalize";
+import { Provider } from "react-redux";
+import { GlobalStyle } from "./GlobalStyle";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { theme } from "./theme";
 import store from "./store";
 
@@ -12,16 +13,16 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
+        <Normalize />
         <GlobalStyle />
         <App />
       </ThemeProvider>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
