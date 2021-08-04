6bfba0f1635a30c658319193375738d58e8e06f2
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-04 14:13:49 +0200

    add stormGray color to theme colors

--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,7 @@ export const theme = {
         white: "#FFFFFF",
         whisper: "#F5F5FA",
         mystic: "#E4E6F0",
+        stormGray: "#74788B",
         woodsmoke: "#18181B",
     },
     breakpoints: {
