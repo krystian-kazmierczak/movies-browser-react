828da188623f1ae27660a90abea237c9af23d1e9
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-04 22:54:59 +0200

    add padding to body

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -2,17 +2,18 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
     html {
-    box-sizing: border-box;
+        box-sizing: border-box;
     }
 
     *,
     ::after,
     ::before {
-    box-sizing: inherit;
+        box-sizing: inherit;
     }
 
     body {
-     font-family: 'Poppins', sans-serif;
-     background-color:${({ theme }) => theme.colors.whisper};
+        font-family: 'Poppins', sans-serif;
+        background-color:${({ theme }) => theme.colors.whisper};
+        padding: 16px;
     }
 `;
