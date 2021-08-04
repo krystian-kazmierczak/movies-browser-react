dde24edb3947fd546657cf904443f52fca03419e
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-04 22:23:33 +0200

    add waterloo color to theme colors

--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,7 @@ export const theme = {
         white: "#FFFFFF",
         whisper: "#F5F5FA",
         mystic: "#E4E6F0",
+        waterloo: "#7E839A",
         stormGray: "#74788B",
         woodsmoke: "#18181B",
     },
