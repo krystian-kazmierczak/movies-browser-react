4374cc805fb012b2eab787c8c4c0cded87010d2b
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-04 13:45:55 +0200

    add Content style, unnecessary comma removed

--- src/common/Tile/index.js
+++ src/common/Tile/index.js
@@ -5,7 +5,7 @@ export const Tile = styled.section`
     padding: 40px;
     box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
     display: grid;
-    grid-template-columns: auto, 1fr;
+    grid-template-columns: auto 1fr;
     grid-gap: 40px;
 `;
 
@@ -13,3 +13,5 @@ export const Image = styled.img`
     width: 312px;
     height: 464px;
 `;
+
+export const Content = styled.div``;
