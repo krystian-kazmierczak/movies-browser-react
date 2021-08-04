be59175c7e1f356bf13b9764f49e1e7003c491eb
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-04 21:58:47 +0200

    add Rating component style

---
+++ src/common/Rating/styled.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    margin: 24px 0;
+`;
+
+export const Star = styled.img`
+    width: 24px;
+    height: 22.87px;
+`;
+
+export const NumericalRating = styled.span`
+    font-weight: 500;
+    font-size: 22px;
+    margin: 0 8px;
+`;
+
+export const Data = styled.span`
+    font-size: 14px;
+    margin-right: 12px;
+`;
