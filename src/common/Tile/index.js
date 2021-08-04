53941ee8b08891a27d75a0c1b5aba37ae55c4768
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-04 13:34:13 +0200

    add grid to Tile style

--- src/common/Tile/index.js
+++ src/common/Tile/index.js
@@ -4,6 +4,9 @@ export const Tile = styled.section`
     background-color: ${({ theme }) => theme.colors.white};
     padding: 40px;
     box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
+    display: grid;
+    grid-template-columns: auto, 1fr;
+    grid-gap: 40px;
 `;
 
 export const Image = styled.img`
