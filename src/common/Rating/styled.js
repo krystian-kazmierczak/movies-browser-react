59cf05e894f5f88d1c2dfb2404a550cf60999ca0
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-05 12:54:31 +0200

    add media queries style

--- src/common/Rating/styled.js
+++ src/common/Rating/styled.js
@@ -4,20 +4,48 @@ export const Wrapper = styled.div`
     display: flex;
     align-items: center;
     margin: 24px 0;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        margin: 12px 0;
+    }
 `;
 
 export const Star = styled.img`
     width: 24px;
     height: 22.87px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        width: 16px;
+        height: 15.25px;
+    }
 `;
 
 export const NumericalRating = styled.span`
     font-weight: 500;
     font-size: 22px;
     margin: 0 8px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        margin: 0 7px 0 8px;
+        font-weight: 600;
+        font-size: 13px;
+        line-height: 17px;
+    }
 `;
 
 export const Data = styled.span`
     font-size: 14px;
     margin-right: 12px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        font-size: 13px;
+        line-height: 17px;
+        color: ${({ theme }) => theme.colors.waterloo};
+    }
+`;
+
+export const Scale = styled(Data)`
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        display: none;
+    }
 `;
--- src/common/Tile/index.js
+++ src/common/Tile/index.js
@@ -7,12 +7,22 @@ export const Tile = styled.section`
     display: grid;
     grid-template-columns: auto 1fr;
     grid-gap: 40px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        padding: 16px;
+        grid-gap: 16px;
+    }
 `;
 
 export const Image = styled.img`
     width: 312px;
     height: 464px;
     border-radius: 5px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        width: 114px;
+        height: 169px;
+    }
 `;
 
 export const Content = styled.div``;
@@ -21,11 +31,26 @@ export const Title = styled.header`
     font-weight: 600;
     font-size: 36px;
     margin: 8px 0 24px 0;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        font-weight: 500;
+        font-size: 16px;
+        line-height: 21px;
+        color: ${({ theme }) => theme.colors.woodsmoke};
+        margin: 0 0 4px 0;
+    }
 `;
 
 export const Subtitle = styled.div`
     font-size: 22px;
     margin-bottom: 24px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        font-size: 13px;
+        line-height: 17px;
+        color: ${({ theme }) => theme.colors.waterloo};
+        margin-bottom: 8px;
+    }
 `;
 
 export const AdditionalInfo = styled.div`
@@ -33,15 +58,28 @@ export const AdditionalInfo = styled.div`
     display: grid;
     grid-template-columns: 1fr;
     grid-gap: 8px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        margin-bottom: 8px;
+    }
 `;
 
 export const InfoDetails = styled.span`
     font-size: 18px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        font-size: 12px;
+        line-height: 16px;
+    }
 `;
 
 export const InfoTitle = styled(InfoDetails)`
     color: ${({ theme }) => theme.colors.stormGray};
     margin-right: 10px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        display: none;
+    }
 `;
 
 export const Tags = styled.ul`
@@ -50,6 +88,10 @@ export const Tags = styled.ul`
     margin: -8px;
     display: flex;
     flex-wrap: wrap;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        margin: -4px;
+    }
 `;
 
 export const Tag = styled.li`
@@ -59,9 +101,22 @@ export const Tag = styled.li`
     padding: 8px 16px;
     margin: 8px;
     border-radius: 5px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        font-size: 10px;
+        line-height: 11px;
+        padding: 4px 8px;
+        margin: 4px;
+    }
 `;
 
 export const Description = styled.p`
     font-size: 20px;
     line-height: 32px;
+    grid-column: 2;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        font-size: 14px;
+        line-height: 22px;
+    }
 `;
