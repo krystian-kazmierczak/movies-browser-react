ae6ae206f1396eb5d0f69f1db3bd46690f85bfeb
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-05 23:43:13 +0200

    add Pager component

---
+++ src/common/Pager/index.js
@@ -0,0 +1,20 @@
+import { Number, Arrow, PagerButton, Text, Wrapper, PageNumberContainer } from "./PagerButton";
+import leftArrow from "../../assets/leftArrow.svg"
+import rightArrow from "../../assets/rightArrow.svg"
+
+const Pager = () => (
+    <Wrapper>
+        <PagerButton><Arrow src={leftArrow} alt="left arrow" />First</PagerButton>
+        <PagerButton><Arrow src={leftArrow} alt="left arrow" />Previous</PagerButton>
+        <PageNumberContainer>
+            <Text>Page</Text>
+            <Number>1</Number>
+            <Text>of</Text>
+            <Number>500</Number>
+        </PageNumberContainer>
+        <PagerButton>Next<Arrow src={rightArrow} alt="right arrow" /></PagerButton>
+        <PagerButton>Last<Arrow src={rightArrow} alt="right arrow" /></PagerButton>
+    </Wrapper>
+);
+
+export default Pager;
