dcbde016bf3adf256b6f3580b51c532bb7040bb3
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-06 14:39:01 +0200

    add media queries to Pager styled.js, change grid template

--- src/common/Pager/styled.js
+++ src/common/Pager/styled.js
@@ -17,7 +17,7 @@ export const PagerButton = styled.button`
     line-height: 20px;
     border: none;
     display: grid;
-    grid-template-columns: repeat(2,auto);
+    grid-auto-flow: column;
     grid-gap: 8px;
     align-items: center;
     
@@ -25,11 +25,22 @@ export const PagerButton = styled.button`
         background-color: ${({ theme }) => theme.colors.cornsilk};
         color: ${({ theme }) => theme.colors.woodsmoke};
     }
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        grid-gap: 4px;
+        padding: 8px 12px;
+        margin-right: 8px;
+    }
 `;
 
 export const Arrow = styled.img`
     width: 7px;
     height: 11px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        width: 5px;
+        height: 8px;
+    }
 `;
 
 export const PageNumberContainer = styled.div`
@@ -37,12 +48,21 @@ export const PageNumberContainer = styled.div`
     grid-gap: 8px;
     grid-template-columns: repeat(4,auto);
     margin: 0 24px 0 12px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        grid-gap: 2px;
+        margin: 0 8px 0 0;
+    }
 `;
 
 export const Text = styled.span`
     font-size: 16px;
     line-height: 24px;
     color: ${({ theme }) => theme.colors.waterloo};
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        font-size: 10px;
+    }
 `;
 
 export const Number = styled(Text)`
