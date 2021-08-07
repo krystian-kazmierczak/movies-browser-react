40b45d3193263c3c496f2a3e42a3bf1d1c155088
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-07 23:51:14 +0200

    add MoviePage , MoviesPage and PeoplePage to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import MoviePage from "./features/MoviePage";
+import MoviesPage from "./features/MoviesPage";
+import PeoplePage from "./features/PeoplePage";
+
 function App() {
   return (
     <>
+      <MoviePage />
+      <MoviesPage />
+      <PeoplePage />
     </>
   );
 }
