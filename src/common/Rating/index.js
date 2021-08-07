0c387a82557f4f76bf03a4597c47bf1cb24a39a6
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-07 22:32:30 +0200

    rename ColumnTile to medium

--- src/common/Rating/index.js
+++ src/common/Rating/index.js
@@ -1,15 +1,15 @@
 import star from "../../assets/star.svg";
 import { Data, NumericalRating, Scale, Star, Wrapper } from "./styled";
 
-export const Rating = ({ rating, votes, columnTile }) => (
-    <Wrapper columnTile={columnTile}>
+export const Rating = ({ rating, votes, medium }) => (
+    <Wrapper medium={medium}>
         <Star src={star}></Star>
-        <NumericalRating columnTile={columnTile}>{rating}</NumericalRating>
-        {!columnTile && (
+        <NumericalRating medium={medium}>{rating}</NumericalRating>
+        {!medium && (
             <Scale>/ 10</Scale>
         )}
         {votes && (
-            <Data columnTile={columnTile}>{votes} votes</Data>
+            <Data medium={medium}>{votes} votes</Data>
         )}
     </Wrapper>
 );
