f40283dc43e9ca8ef4287da5f2bcf0ce1ac4bafd
Krystian Kaźmierczak <s155774@gmail.com>
2021-08-08 20:34:21 +0200

    fix mobile view

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -5,8 +5,9 @@ import {
   NavigationLeftWrapper,
   NavigationRightWrapper,
   List,
-  ListItem,
+  Item,
   StyledNavLink,
+  NavigationWrapper,
 } from "./styled";
 import logo from "../assets/logo.svg";
 import { Search } from "../features/Search";
@@ -14,22 +15,25 @@ import { Search } from "../features/Search";
 export const Navigation = () => {
   return (
     <NavigationContainer>
-      <NavigationLeftWrapper>
-        <LogoWrapper>
-          <Logo src={logo} />
-        </LogoWrapper>
-        <List>
-          <ListItem>
-            <StyledNavLink to="/movies">MOVIES</StyledNavLink>
-          </ListItem>
-          <ListItem>
-            <StyledNavLink to="/people">PEOPLE</StyledNavLink>
-          </ListItem>
-        </List>
-      </NavigationLeftWrapper>
-      <NavigationRightWrapper>
-        <Search />
-      </NavigationRightWrapper>
+      <NavigationWrapper>
+        <NavigationLeftWrapper>
+          <LogoWrapper>
+            <Logo src={logo} alt="" />
+            movies&nbsp;browser
+          </LogoWrapper>
+          <List>
+            <Item>
+              <StyledNavLink to="/movies">movies</StyledNavLink>
+            </Item>
+            <Item>
+              <StyledNavLink to="/people">people</StyledNavLink>
+            </Item>
+          </List>
+        </NavigationLeftWrapper>
+        <NavigationRightWrapper>
+          <Search />
+        </NavigationRightWrapper>
+      </NavigationWrapper>
     </NavigationContainer>
   );
 };
--- src/Navigation/styled.js
+++ src/Navigation/styled.js
@@ -1,54 +1,68 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
-import { Link, NavLink } from "react-router-dom";
 
 const activeClassName = "link-active";
 
 export const NavigationContainer = styled.nav`
-  display: flex;
-  justify-content: space-around;
-  flex-wrap: wrap;
-  align-items: center;
-  min-height: 94px;
-  padding: 23px 10px 0 20px;
   color: ${({ theme }) => theme.colors.white};
   background: ${({ theme }) => theme.colors.black};
+  margin: 0;
+  padding: 23px 16px;
+  min-height: 94px;
+`;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
-    padding-bottom: 0;
+export const NavigationWrapper = styled.div`
+  max-width: 1368px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-gap: 32px;
+  align-items: center;
+  justify-content: space-between;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletLandscape}px) {
+    grid-template-columns: 1fr;
+    justify-content: center;
   }
 `;
 
 export const NavigationLeftWrapper = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
 `;
 
 export const NavigationRightWrapper = styled.div`
-  padding: 23px;
+  padding: 0 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const List = styled.ul`
   display: flex;
   margin: 0;
+  padding: 0 16px;
   list-style: none;
 `;
 
-export const ListItem = styled.li`
+export const Item = styled.li`
   text-decoration: none;
+  text-transform: uppercase;
+  padding-left: 16px;
 `;
 
-export const StyledNavLink = styled(NavLink).attrs(() => ({
-  activeClassName,
-}))`
-  margin: 0 10px;
-  padding: 13px 24px;
+export const StyledNavLink = styled(NavLink).attrs(() => ({ activeClassName }))`
+  padding: 14px 16px;
   font-size: 14px;
   font-weight: 600;
+  line-height: 1.5;
   color: ${({ theme }) => theme.colors.white};
   text-decoration: none;
-  border-radius: 33px;
+  border-radius: 24px;
   transition: 0.5s;
-
+  outline: none;
+  
   &:hover {
     color: ${({ theme }) => theme.colors.black};
     background: ${({ theme }) => theme.colors.white};
@@ -65,52 +79,55 @@ export const StyledNavLink = styled(NavLink).attrs(() => ({
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
-    margin: 0 3px;
-    padding: 10px 17px;
+    border-radius: 27px;
     font-size: 13px;
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
-    margin: 0 8px;
-    padding: 8px 12px;
+    border-radius: 29px;
     font-size: 12px;
   }
 `;
 
-export const LogoWrapper = styled(Link)`
+export const LogoWrapper = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: nowrap;
+  padding: 0 16px;
   margin: 0;
-  padding-left: 16px;
   color: ${({ theme }) => theme.colors.white};
   font-size: 24px;
   font-weight: 500;
   text-decoration: none;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.desktopSmall}px) {
-    margin-right: 30px;
-  }
+  line-height: 1.7;
+  letter-spacing: -1.5px;
+  text-transform: capitalize;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
-    margin-right: 22px;
-    padding: 10px;
+    letter-spacing: -1px;
+    line-height: 1.5;
     font-size: 18px;
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
-    margin-right: 19px;
-    padding: 0;
+    line-height: 1.3;
+    letter-spacing: -0.5px;
     font-size: 13px;
   }
 `;
 
 export const Logo = styled.img`
   height: 40px;
+  padding: 0 16px 0 0;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
     height: 30px;
+    padding: 0 10px 0 0;
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
     height: 20px;
+    padding: 0 4px 0 0;
   }
 `;
