e8dc565d8c4b5a2ed7e387fd2e195f9d12bd059a
Kinga Parasiewicz <kingaparasiewicz1997@gmail.com>
2021-08-09 10:44:14 +0200

    create DynamicBox with styled

---
+++ src/features/Search/DynamicBox/index.js
@@ -0,0 +1,62 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { language, apiKey, apiBase } from "../../../common/commonValues";
+import { Loading } from "./../../../common/Loading/index";
+import {
+  selectResults,
+  setActiveSearchPath,
+  selectLoading,
+} from "./../SearchSlice";
+import { Wrapper } from "./styled";
+import { NoResult } from "./../../../common/NoResult/index";
+import noPoster from "./../../../assets/noPoster.svg";
+import noProfile from "./../../../assets/noProfile.svg";
+
+export const DynamicBox = ({ query }) => {
+  const dispatch = useDispatch();
+  const results = useSelector(selectResults);
+  const loading = useSelector(selectLoading);
+  const location = useLocation();
+  const pathText = location.pathname.includes("movie") ? "movie" : "person";
+
+  useEffect(() => {
+    dispatch(
+      setActiveSearchPath(
+        `${apiBase}search/${pathText}${apiKey}${language}&query=${query}`
+      )
+    );
+  }, [query, dispatch, pathText]);
+
+  return (
+    <Wrapper>
+      {loading ? (
+        <Loading />
+      ) : !results.length ? (
+        <NoResult urlQuery={query} />
+      ) : (
+        results.map((result) => (
+          <div
+            key={result.id}
+            pathText={pathText}
+            {...(pathText === "movie"
+              ? {
+                  id: result.id,
+                  text: result.title,
+                  image: result.poster_path
+                    ? `https://image.tmdb.org/t/p/w154${result.poster_path}`
+                    : noPoster,
+                }
+              : {
+                  id: result.id,
+                  text: result.name,
+                  image: result.profile_path
+                    ? `https://image.tmdb.org/t/p/w185${result.profile_path}`
+                    : noProfile,
+                })}
+          />
+        ))
+      )}
+    </Wrapper>
+  );
+};
