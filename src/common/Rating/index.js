888f92d2b64b735b253e1d00bdbd156aa83596e6
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-09 13:26:19 +0200

    add cover view to Rating component

--- src/common/Rating/index.js
+++ src/common/Rating/index.js
@@ -1,15 +1,51 @@
+import { BrowserView, MobileView } from "react-device-detect";
 import star from "../../assets/star.svg";
-import { Data, NumericalRating, Scale, Star, Wrapper } from "./styled";
+import { Data, NumericalRating, Scale, Star, Wrapper, RatingContainer } from "./styled";
 
-export const Rating = ({ rating, votes, medium }) => (
-    <Wrapper medium={medium}>
-        <Star src={star}></Star>
-        <NumericalRating medium={medium}>{rating}</NumericalRating>
-        {!medium && (
-            <Scale>/ 10</Scale>
-        )}
-        {votes && (
-            <Data medium={medium}>{votes} votes</Data>
-        )}
-    </Wrapper>
-);
+export const Rating = ({ rating, votes, medium, cover }) => {
+    if (cover) {
+        return (
+            <>
+                <BrowserView>
+                    <Wrapper cover={cover}>
+                        <RatingContainer>
+                            <Star src={star} cover={cover}></Star>
+                            <NumericalRating cover={cover}>{rating}</NumericalRating>
+                            {!medium && (
+                                <Scale cover={cover}>/ 10</Scale>
+                            )}
+                        </RatingContainer>
+                        {votes && (
+                            <Data cover={cover}>{votes} votes</Data>
+                        )}
+                    </Wrapper>
+                </BrowserView>
+                <MobileView>
+                    <Wrapper cover={cover}>
+                        <Star src={star} cover={cover}></Star>
+                        <NumericalRating cover={cover}>{rating}</NumericalRating>
+                        {!medium && (
+                            <Scale cover={cover}>/ 10</Scale>
+                        )}
+                        {votes && (
+                            <Data cover={cover}>{votes} votes</Data>
+                        )}
+                    </Wrapper>
+                </MobileView>
+            </>
+        )
+    }
+
+    return (
+        <Wrapper medium={medium}>
+            <Star src={star}></Star>
+            <NumericalRating medium={medium}>{rating}</NumericalRating>
+            {!medium && (
+                <Scale>/ 10</Scale>
+            )}
+            {votes && (
+                <Data medium={medium}>{votes} votes</Data>
+            )}
+        </Wrapper>
+    );
+};
--- src/common/Rating/styled.js
+++ src/common/Rating/styled.js
@@ -5,23 +5,45 @@ export const Wrapper = styled.div`
     align-items: center;
     margin: 24px 0;
 
-    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
         margin: 12px 0 0 0;
     }
 
     ${({ medium }) => medium && css`
         margin: 8px 0 0 0;
     `}
+
+    ${({ cover }) => cover && css`
+        margin: 25px 0 56px 0;
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-gap: 16px;
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+            display: flex;
+            align-items: center;
+            grid-gap: 0;
+            margin: 0 0 8px 0;
+        }
+    `}
+
 `;
+
+export const RatingContainer = styled.div``;
 
 export const Star = styled.img`
     width: 24px;
     height: 22.87px;
 
-    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
         width: 16px;
         height: 15.25px;
     }
+
+    ${({ cover }) => cover && css`
+        width: 40px;
+        height: 38.12px;
+    `}
 `;
 
 export const NumericalRating = styled.span`
@@ -29,7 +51,7 @@ export const NumericalRating = styled.span`
     font-size: 22px;
     margin: 0 8px;
 
-    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
         margin: 0 7px 0 8px;
         font-weight: 600;
         font-size: 13px;
@@ -42,13 +64,24 @@ export const NumericalRating = styled.span`
         line-height: 24px;
         margin: 0 12px;
     `}
+
+    ${({ cover }) => cover && css`
+        font-size: 30px;
+        line-height: 39px;
+        color: ${({ theme }) => theme.colors.white};
+        margin: 0 7px 0 8px;
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+            margin: 0 2px 0 4px;
+        }
+    `}
 `;
 
 export const Data = styled.span`
     font-size: 14px;
     margin-right: 12px;
 
-    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
         font-size: 13px;
         line-height: 17px;
         color: ${({ theme }) => theme.colors.waterloo};
@@ -59,10 +92,31 @@ export const Data = styled.span`
         line-height: 24px;
         color: ${({ theme }) => theme.colors.waterloo};
     `}
+
+    ${({ cover }) => cover && css`
+        font-size: 16px;
+        line-height: 19px;
+        color: ${({ theme }) => theme.colors.white};
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+            font-size: 10px;
+            line-height: 12px;
+            color: ${({ theme }) => theme.colors.white};
+            margin-right: 8px;
+        }
+    `}
+
+
 `;
 
 export const Scale = styled(Data)`
-    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
         display: none;
     }
+
+    ${({ cover }) => cover && css`
+        @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+            display: unset;
+        }
+    `}
 `;
