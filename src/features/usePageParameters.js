f7cfab49039003264a35be52bb72e96818f253d3
Kinga Parasiewicz <kingaparasiewicz1997@gmail.com>
2021-08-09 10:44:37 +0200

    create usePageParameters.js

---
+++ src/features/usePageParameters.js
@@ -0,0 +1,32 @@
+import { useHistory, useLocation } from "react-router-dom";
+
+export const usePageParameter = (paramName) => {
+  const location = useLocation();
+
+  return new URLSearchParams(location.search).get(paramName);
+};
+
+export const useReplacePageParameters = () => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const searchParams = new URLSearchParams(location.search);
+
+  const replacePageParameters = (parameters) => {
+    parameters.forEach(({ key, value }) => {
+      if (value === "") {
+        searchParams.delete(key);
+      } else {
+        searchParams.set(key, value);
+      }
+    });
+
+    history.push(
+      `${
+        location.pathname.includes("movie") ? "/movies" : "/people"
+      }?${searchParams.toString()}`
+    );
+  };
+
+  return replacePageParameters;
+};
