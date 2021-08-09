4e5f410cdbf425ebd5f0cbfe6cabce46f3425d01
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-09 22:43:44 +0200

    remove padding from body

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -14,6 +14,5 @@ export const GlobalStyle = createGlobalStyle`
     body {
         font-family: 'Poppins', sans-serif;
         background-color:${({ theme }) => theme.colors.whisper};
-        padding: 16px;
     }
 `;
