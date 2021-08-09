cd2cb1192b93b6a53e05e189ac53e2c63adebecd
Kinga Parasiewicz <kingaparasiewicz1997@gmail.com>
2021-08-09 10:45:37 +0200

    add component DynamicBox and usePageParameters to Search and create function onFormSubmit

--- src/features/Search/index.js
+++ src/features/Search/index.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
 import { selectOpen, selectQuery, setOpen, setQuery } from "./SearchSlice";
 import { SearchWrapper, Input, StyledButtonSearch } from "./styled";
+import { DynamicBox } from "./DynamicBox/index";
+import { useReplacePageParameters } from "../usePageParameters";
 
 export const Search = () => {
   const query = useSelector(selectQuery);
@@ -10,6 +12,7 @@ export const Search = () => {
   const dispatch = useDispatch();
   const location = useLocation();
   const searchText = location.pathname.includes("movie") ? "movies" : "people";
+  const replacePageParameters = useReplacePageParameters();
 
   useEffect(() => {
     dispatch(setQuery(""));
@@ -20,15 +23,31 @@ export const Search = () => {
     dispatch(setOpen(true));
   };
 
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    replacePageParameters([
+        {
+            key: "search",
+            value: query,
+        },
+        {
+            key: "page",
+            value: 1,
+        }
+    ]);
+    dispatch(setQuery(""));
+};
   return (
-    <SearchWrapper>
+    <SearchWrapper onSubmit={onFormSubmit}>
       <StyledButtonSearch />
       <Input
-        placeholder={`Search for ... ${searchText}`}
+        placeholder={`Search for ${searchText} ...`}
         value={query}
         open={isOpen}
         onChange={({ target }) => onChange(target.value)}
       />
+      {query && isOpen && <DynamicBox query={query} />}
     </SearchWrapper>
   );
 };
