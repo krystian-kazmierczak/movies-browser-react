139fc5d49b2ff6b394cfe5c7b21bd7f6e61caf2b
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-10 23:24:32 +0200

    change Pages to use new Big/Medium/SmallTile components

--- src/features/MoviesPage/index.js
+++ src/features/MoviesPage/index.js
@@ -1,50 +1,16 @@
-import { MobileView, BrowserView } from 'react-device-detect';
-import { Rating } from "../../common/Rating";
-import { Content, Image, Subtitle, Tag, Tags, Tile, Title } from "../../common/Tile";
-import noPoster from "../../assets/noPoster.svg";
+import { MediumTile } from "../../common/Tile/MediumTile";
+import { movieData } from "../../movieData";
 
 const MoviesPage = () => {
     return (
-        <>
-            <BrowserView>
-                <Tile medium>
-                    <Content>
-                        <Image medium src={noPoster} />
-                        <Title medium>Mulan long title long title long long</Title>
-                        <Subtitle medium>2020</Subtitle>
-                        <Tags medium>
-                            <Tag medium>Action</Tag>
-                            <Tag medium>Adventure</Tag>
-                            <Tag medium>Drama</Tag>
-                        </Tags>
-                    </Content>
-                    <Rating
-                        medium={true}
-                        rating="7,8"
-                        votes="335"
-                    />
-                </Tile>
-            </BrowserView>
-            <MobileView>
-                <Tile medium>
-                    <Image medium src={noPoster} />
-                    <Content>
-                        <Title medium>Mulan long title long title long long</Title>
-                        <Subtitle medium>2020</Subtitle>
-                        <Tags medium>
-                            <Tag medium>Action</Tag>
-                            <Tag medium>Adventure</Tag>
-                            <Tag medium>Drama</Tag>
-                        </Tags>
-                        <Rating
-                            medium={true}
-                            rating="7,8"
-                            votes="335"
-                        />
-                    </Content>
-                </Tile>
-            </MobileView>
-        </>
+        <MediumTile
+            imageSrc={movieData.poster}
+            title={movieData.title}
+            subtitle={movieData.subtitle}
+            tags={movieData.tags}
+            rating={movieData.rating}
+            votes={movieData.votes}
+        />
     );
 };
 
