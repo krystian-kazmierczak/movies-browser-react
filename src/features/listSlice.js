95df11ff6b9631c7a05f055952c8eb4de235f7ea
Kinga Parasiewicz <kingaparasiewicz1997@gmail.com>
2021-08-11 10:00:14 +0200

    create reducers, action creator and states

--- src/features/listSlice.js
+++ src/features/listSlice.js
@@ -1,1 +1,49 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const listSlice = createSlice({
+  name: "list",
+  initialState: {
+    list: [],
+    loading: true,
+    numberOfPages: undefined,
+    error: false,
+    totalResults: undefined,
+  },
+  reducers: {
+    fetchList: (state) => {
+      state.loading = true;
+    },
+    fetchListSuccess: (state, { payload: list }) => {
+      state.list = list.results;
+      state.loading = false;
+      state.error = false;
+      state.totalResults = list.totalResults;
+      state.numberOfPages = list.totalPage;
+    },
+    fetchListError: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+    resetState: (state) => {
+      state.list = [];
+      state.loading = true;
+      state.error = false;
+      state.numberOfPages = undefined;
+      state.totalResults = undefined;
+    },
+  },
+});
+
+export const {
+  fetchList,
+  fetchListError,
+  fetchListSuccess,
+  resetState,
+} = listSlice.actions;
+
+export const selectList = (state) => state.list;
+export const selectLoading = (state) => state.list.loading;
+export const selectError = (state) => state.list.error;
+export const selectNumberOfPages = (state) => state.list.numberOfPages;
+
+export default listSlice.reducer;
