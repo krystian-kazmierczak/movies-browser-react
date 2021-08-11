39d0e67e2f0d61775492923303097a6a19fb942c
Kinga Parasiewicz <kingaparasiewicz1997@gmail.com>
2021-08-11 10:00:39 +0200

    add listSlice to store

--- src/store.js
+++ src/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./rootSaga";
 import searchReducer from "./features/Search/SearchSlice";
+import listReducer from "./features/listSlice";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
     search: searchReducer,
+    list: listReducer,
   },
   middleware: [sagaMiddleware],
 });
