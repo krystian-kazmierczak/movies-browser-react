980e00aa265ec8bd461126d8ce7b045977d461ac
Kinga Parasiewicz <kingaparasiewicz1997@gmail.com>
2021-08-12 14:09:52 +0200

    add ItemSaga to rootSaga.js

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -2,11 +2,13 @@ import { all } from "redux-saga/effects";
 import { watchSetActiveSearchPath } from "./features/Search/SearchSaga";
 import { watchfetchCommon } from "./common/commonSaga";
 import { watchFetchList } from "./features/listSaga";
+import { watchFetchItem } from "./features/itemSaga";
 
 export default function* rootSaga() {
   yield all([
     watchSetActiveSearchPath(),
     watchfetchCommon(),
-    watchFetchList()
+    watchFetchList(),
+    watchFetchItem()
   ]);
 }
