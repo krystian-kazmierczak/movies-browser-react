567fe9584cea0f652454a9445e084127728e07ed
Krystian Kaźmierczak <s155774@gmail.com>
2021-08-12 15:53:57 +0200

    add breakpoints

--- src/features/ProfilePage/styled.js
+++ src/features/ProfilePage/styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Section = styled.section`
   display: flex;
   flex-wrap: wrap;
-  gap: 24px;
+  gap: 12px;
   width: 100%;
   max-width: 1368px;
 `;
@@ -12,5 +12,15 @@ export const SectionTitle = styled.h2`
   font-style: normal;
   font-weight: 600;
   font-size: 36px;
-  line-height: 120%;
+  line-height: 1.2;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+    font-size: 28px;
+    gap: 10px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    font-size: 20px;
+    gap: 8px;
+  }
 `;
