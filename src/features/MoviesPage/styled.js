3d780a636109e0460dbac71972dadbb7705856ae
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-12 14:12:11 +0200

    add media query

--- src/features/MoviesPage/styled.js
+++ src/features/MoviesPage/styled.js
@@ -6,10 +6,20 @@ export const Header = styled.h1`
   font-size: 36px;
   line-height: 1.2;
   color: ${({ theme }) => theme.colors.woodsmoke};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+    font-size: 18px;
+    margin: 8px 0 12px 0;
+  }
 `;
 
 export const TilesWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(4,auto);
   grid-gap: 24px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+    grid-template-columns: 1fr;
+    grid-gap: 16px;
+  }
 `;
