314176448bd1bcbc410f4fa625a77f620a6a8ee4
Kinga Parasiewicz <kingaparasiewicz1997@gmail.com>
2021-08-12 10:15:10 +0200

    add commonSaga and ListSaga to rootSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,6 +1,12 @@
 import { all } from "redux-saga/effects";
 import { watchSetActiveSearchPath } from "./features/Search/SearchSaga";
+import { watchfetchCommon } from "./common/commonSaga";
+import { watchFetchList } from "./features/listSaga";
 
 export default function* rootSaga() {
-  yield all([watchSetActiveSearchPath()]);
+  yield all([
+    watchSetActiveSearchPath(),
+    watchfetchCommon(),
+    watchFetchList()
+  ]);
 }
