2724ebb70d7b0170f9759e99c227b2a4d3157de8
Krystian Kaźmierczak <s155774@gmail.com>
2021-08-12 15:31:18 +0200

    create component MediumTile

---
+++ src/features/ProfilePage/MediumTile/index.js
@@ -0,0 +1,77 @@
+import { MobileOnlyView, TabletView, BrowserView } from "react-device-detect";
+import { Rating } from "../../../common/Rating";
+import {
+  Content,
+  Image,
+  Subtitle,
+  Tag,
+  Tags,
+  Tile,
+  Title,
+} from "../../../common/Tile/styled";
+
+export const MediumTile = ({
+  src,
+  title,
+  subtitle,
+  subtitle2,
+  tags,
+  rating,
+  votes,
+  genres,
+}) => (
+  <>
+    <BrowserView>
+      <Tile medium>
+        <Content>
+          <Image medium src={src} />
+          <Title medium>{title}</Title>
+          <Subtitle medium>{subtitle}</Subtitle>
+          <Subtitle medium>{subtitle2}</Subtitle>
+          <Tags medium>
+            {tags.map((tag) => (
+              <Tag key={tag} medium>
+                {genres[tag.toString()]}
+              </Tag>
+            ))}
+          </Tags>
+        </Content>
+        <Rating medium={true} rating={rating} votes={votes} />
+      </Tile>
+    </BrowserView>
+    <TabletView>
+      <Tile medium>
+        <Content>
+          <Image medium src={src} />
+          <Title medium>{title}</Title>
+          <Subtitle medium>{subtitle}</Subtitle>
+          <Tags medium>
+            {tags.map((tag) => (
+              <Tag key={tag} medium>
+                {genres[tag.toString()]}
+              </Tag>
+            ))}
+          </Tags>
+        </Content>
+        <Rating medium={true} rating={rating} votes={votes} />
+      </Tile>
+    </TabletView>
+    <MobileOnlyView>
+      <Tile medium>
+        <Image medium src={src} />
+        <Content>
+          <Title medium>{title}</Title>
+          <Subtitle medium>{subtitle}</Subtitle>
+          <Tags medium>
+            {tags.map((tag) => (
+              <Tag key={tag} medium>
+                {genres[tag.toString()]}
+              </Tag>
+            ))}
+          </Tags>
+          <Rating medium={true} rating={rating} votes={votes} />
+        </Content>
+      </Tile>
+    </MobileOnlyView>
+  </>
+);
