3d6d74038423e999d5143b47bbe12e11fcc79e57
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-12 13:15:05 +0200

    change view to MoviePage only

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import { HashRouter } from "react-router-dom";
 import { Navigation } from "./Navigation";
 import { MainContainer } from "./styled";
-import Cover from "./common/Cover";
-import MoviePage from "./features/MoviePage";
 import MoviesPage from "./features/MoviesPage";
-import PeoplePage from "./features/PeoplePage";
+
 
 export const App = () => (
   <HashRouter>
     <Navigation />
-    <Cover />
     <MainContainer>
-      <MoviePage />
       <MoviesPage />
-      <PeoplePage />
     </MainContainer>
   </HashRouter>
 );
