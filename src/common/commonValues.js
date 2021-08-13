4fe4e6f82f17cca7ab2bd4cdc84fdf9fd524062f
Krystian Kaźmierczak <s155774@gmail.com>
2021-08-13 10:16:31 +0200

    remove unnecessary export in commonValues.js

--- src/common/commonValues.js
+++ src/common/commonValues.js
@@ -2,4 +2,3 @@ export const language = "&language=en-US";
 export const apiBase = "https://api.themoviedb.org/3/";
 export const apiImage = "https://image.tmdb.org/t/p/"
 export const apiKey = "a77eedf852ecdfae179d1d5e10c411dd";
-export const apiImage = "https://image.tmdb.org/t/p/"
