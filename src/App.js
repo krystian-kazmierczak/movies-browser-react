1d934373411c9066d972ff7ba10c7d5ca7adcdf8
Krystian Kaźmierczak <s155774@gmail.com>
2021-08-14 21:36:34 +0200

    add route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-import { HashRouter } from "react-router-dom";
+import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
 import { Navigation } from "./Navigation";
 import { MainContainer } from "./styled";
 import ProfilePage from "./features/ProfilePage";
-import { examplePerson as data, exampleMovieCredits as movies} from "./features/ProfilePage/exampleData";
+import {
+  examplePerson as data,
+  exampleMovieCredits as movies,
+} from "./features/ProfilePage/exampleData";
 import { apiImage, apiKey } from "./common/commonValues";
+import MoviesPage from "./features/MoviesPage";
+import { Error } from "./common/Error";
 
-const imageApi =`${apiImage}/w500${data.profile_path}?api_key=${apiKey}`;
+const imageApi = `${apiImage}/w500${data.profile_path}?api_key=${apiKey}`;
 
 export const App = () => (
   <HashRouter>
     <Navigation />
     <MainContainer>
-      <ProfilePage name={data.name} birthDay={data.birthday} placeOfBirth={data.place_of_birth} description={data.biography} poster={imageApi} cast={movies.cast}/>
+      <Switch>
+        <Route path="/movies">
+          <MoviesPage />
+        </Route>
+        <Route path="/people">
+          <ProfilePage
+            name={data.name}
+            birthDay={data.birthday}
+            placeOfBirth={data.place_of_birth}
+            description={data.biography}
+            poster={imageApi}
+            cast={movies.cast}
+          />
+        </Route>
+        <Route path="/error">
+          <Error />
+        </Route>
+        <Route path="/">
+          <Redirect to="/movies" />
+        </Route>
+      </Switch>
     </MainContainer>
   </HashRouter>
 );
