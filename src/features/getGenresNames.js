843a6b43ea45bfc551de63e32aa3afa516b95dc3
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-16 20:44:55 +0200

    add condition to getGenreNames

--- src/features/getGenresNames.js
+++ src/features/getGenresNames.js
@@ -1,4 +1,6 @@
 export const getGenreNames = (genre_ids, genres) => {
-    const string = genre_ids.map(tag => `${ genres.find(({ id }) => id === tag).name}`).join(",");
-    return string.split(",");
+    if (genre_ids.length > 0) {
+        const string = genre_ids.map(tag => `${genres.find(({ id }) => id === tag).name}`).join(",");
+        return string.split(",");
+    }
 };
