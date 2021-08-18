cfe4aa17563b71eb3cd61b9d72ea2c23df60c045
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-18 16:55:36 +0200

    add ProfilePage to App

--- src/App.js
+++ src/App.js
@@ -4,27 +4,30 @@ import MoviesPage from "./features/MoviesPage";
 import PeoplePage from "./features/PeoplePage";
 import MoviePage from "./features/MoviePage";
 import { Error } from "./common/Error";
-import { toError, toMovie, toMovies, toPeople } from "./common/routes";
+import { toError, toMovie, toMovies, toPeople, toProfile } from "./common/routes";
 
 export const App = () => (
   <HashRouter>
     <Navigation />
-      <Switch>
-        <Route path={toMovie()}>
-          <MoviePage />
-        </Route>
-        <Route path={toMovies()}>
-          <MoviesPage />
-        </Route>
-        <Route path={toPeople()}>
-          <PeoplePage />
-        </Route>
-        <Route path={toError()}>
-          <Error />
-        </Route>
-        <Route>
-          <Redirect to={toMovies()} />
-        </Route>
-      </Switch>
+    <Switch>
+      <Route path={toMovie()}>
+        <MoviePage />
+      </Route>
+      <Route path={toMovies()}>
+        <MoviesPage />
+      </Route>
+      <Route path={toProfile()}>
+        <ProfilePage />
+      </Route>
+      <Route path={toPeople()}>
+        <PeoplePage />
+      </Route>
+      <Route path={toError()}>
+        <Error />
+      </Route>
+      <Route>
+        <Redirect to={toMovies()} />
+      </Route>
+    </Switch>
   </HashRouter>
 );
