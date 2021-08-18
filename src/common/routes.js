ca10e5bfec54ec021a0cfb44d3d704f1a03bdf7e
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-18 16:54:59 +0200

    rename toPerson to toProfile

--- src/common/routes.js
+++ src/common/routes.js
@@ -3,4 +3,4 @@ export const toPeople = () => "/people";
 export const toError = () => "/error";
 
 export const toMovie = ({ id } = { id: ":id" }) => `/movies/${id}`;
-export const toPerson = ({ id } = { id: ":id" }) => `/people/${id}`;
+export const toProfile = ({ id } = { id: ":id" }) => `/people/${id}`;
