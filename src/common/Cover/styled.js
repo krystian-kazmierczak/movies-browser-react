0bdce0d2f69ce26fda702c99ba06cda2b46a3dea
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-18 16:17:01 +0200

    change cover style

--- src/common/Cover/styled.js
+++ src/common/Cover/styled.js
@@ -1,17 +1,22 @@
 import styled from "styled-components";
-import posterBig from "../../images/posterBig.png"
 
 export const Header = styled.header`
     background: linear-gradient(270deg, #000000 14.11%, rgba(0, 0, 0, 0.873268) 15.08%, rgba(0, 0, 0, 0.720529) 16.51%, rgba(0, 0, 0, 0.294577) 19.99%, rgba(0, 0, 0, 0.159921) 21.88%, rgba(0, 0, 0, 0) 25.68%), linear-gradient(90deg, #000000 13.6%, rgba(0, 0, 0, 0.984059) 14.58%, rgba(0, 0, 0, 0.967732) 15.44%, rgba(0, 0, 0, 0.865569) 16.3%, rgba(0, 0, 0, 0.230315) 22.87%, rgba(0, 0, 0, 0) 26.64%), linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0.689555) 4.66%, rgba(0, 0, 0, 0.439033) 9.34%, rgba(0, 0, 0, 0.20628) 15.16%, rgba(0, 0, 0, 0) 24.22%), linear-gradient(189.44deg, rgba(0, 0, 0, 0) 58.48%, rgba(0, 0, 0, 0.106473) 63.17%, rgba(0, 0, 0, 0.235359) 68.85%, rgba(0, 0, 0, 0.492821) 78.08%, rgba(0, 0, 0, 0.740286) 85.86%, #000000 92.87%), 
-                url(${posterBig});
+                url(${({ url }) => url});
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
     min-height: 770px;
+    height: 70vh;
 
     display: flex;
     justify-content: flex-start;
     align-items: flex-end;
+    
+    @media (max-width: ${({ theme }) => theme.breakpoints.desktopSmall}px) {
+        min-height: 40vh;
+        background-size: contain;
+    }
 
     @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
         min-height: 200px;
@@ -25,6 +30,7 @@ export const Header = styled.header`
 `;
 
 export const Wrapper = styled.div`
+    max-width: 1368px;
     width: 80%;
     padding-left: 16px;
     margin: 0 auto;
