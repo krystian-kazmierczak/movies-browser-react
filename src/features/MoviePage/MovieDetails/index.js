032d08f00c792a0610e4036cc1fc9f736cff89bc
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-18 12:30:25 +0200

    add StyledLink to crew and cast

--- src/features/MoviePage/MovieDetails/index.js
+++ src/features/MoviePage/MovieDetails/index.js
@@ -5,6 +5,8 @@ import { SmallTile } from "../../../common/Tile/SmallTile";
 import { Header, Section } from "../../../common/Container/index";
 import noProfile from "../../../assets/noProfile.svg";
 import { selectAdditionalData } from "../../itemSlice";
+import { toPerson } from "../../../common/routes";
+import { StyledLink } from "../../../common/StyledLink";
 
 const MovieDetails = () => {
     const movieAdditionalData = useSelector(selectAdditionalData);
@@ -14,31 +16,35 @@ const MovieDetails = () => {
             <Header>Cast</Header>
             <Section>
                 {movieAdditionalData.cast.map((person) => (
-                    <SmallTile
-                        key={nanoid()}
-                        src={
-                            !!person.profile_path
-                                ? `${apiImage}/w500${person.profile_path}?api_key=${apiKey}`
-                                : noProfile
-                        }
-                        title={person.name}
-                        subtitle={person.character}
-                    />
+                    <StyledLink key={nanoid()} to={toPerson({ id: person.id })}>
+                        <SmallTile
+                            key={nanoid()}
+                            src={
+                                !!person.profile_path
+                                    ? `${apiImage}/w500${person.profile_path}?api_key=${apiKey}`
+                                    : noProfile
+                            }
+                            title={person.name}
+                            subtitle={person.character}
+                        />
+                    </StyledLink>
                 ))}
             </Section>
             <Header>Crew</Header>
             <Section>
                 {movieAdditionalData.crew.map((person) => (
-                    <SmallTile
-                        key={nanoid()}
-                        src={
-                            !!person.profile_path
-                                ? `${apiImage}/w500${person.profile_path}?api_key=${apiKey}`
-                                : noProfile
-                        }
-                        title={person.name}
-                        subtitle={person.job}
-                    />
+                    <StyledLink key={nanoid()} to={toPerson({ id: person.id })}>
+                        <SmallTile
+                            key={nanoid()}
+                            src={
+                                !!person.profile_path
+                                    ? `${apiImage}/w500${person.profile_path}?api_key=${apiKey}`
+                                    : noProfile
+                            }
+                            title={person.name}
+                            subtitle={person.job}
+                        />
+                    </StyledLink>
                 ))}
             </Section>
         </>
