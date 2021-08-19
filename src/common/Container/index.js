a7eca5f7b8b2b7cd1337a5556c1afaed6ab0b443
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-19 23:09:50 +0200

    add side padding to Container media queries

--- src/common/Container/index.js
+++ src/common/Container/index.js
@@ -3,6 +3,10 @@ import styled from "styled-components";
 export const Container = styled.div`
   max-width: 1368px;
   margin: auto;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+    padding: 0 16px;
+  }
 `;
 
 export const Section = styled.section`
