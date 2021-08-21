e1ac84a96a077928fb5c2d13c1b8ea49d7641c49
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-21 23:06:11 +0200

    add switch logic to generate section type

--- src/common/Section/index.js
+++ src/common/Section/index.js
@@ -6,22 +6,24 @@ import { toMovie, toProfile } from "../routes";
 import { StyledLink } from "../StyledLink";
 import { MediumTile } from "../Tile/MediumTile";
 import { SmallTile } from "../Tile/SmallTile";
+import { Arrow, Button, Label } from "../Button";
 import { StyledSection } from "./styled";
 import { selectAdditionalData } from "../../features/itemSlice";
+import { selectList } from "../../features/listSlice";
 import { getYearFromDate } from "../../features/getYearFromDate";
 import { getGenreNames } from "../../features/getGenresNames";
 import { genres } from "../../features/genres";
-import { Arrow, Button, Label } from "../Button";
 import noProfile from "../../assets/noProfile.svg";
 import noPoster from "../../assets/noPoster.svg";
 import downArrowBlue from "../../assets/downArrowBlue.svg";
 import upArrowBlue from "../../assets/upArrowBlue.svg";
 
-
-const Section = ({ movieDetails, profileDetails, crew }) => {
+const Section = ({ type, crew }) => {
   const [showMore, setShowMore] = useState(false);
+  const resultsPage = useSelector(selectList);
   const additionalData = useSelector(selectAdditionalData);
-  const hideIndex = movieDetails ? 5 : 3;
+  const hideIndex = type === "movie" ? 5 : 3;
+
   let data;
   if (crew) {
     data = additionalData.crew
@@ -29,49 +31,96 @@ const Section = ({ movieDetails, profileDetails, crew }) => {
     data = additionalData.cast
   }
 
-  return (
-    <>
-      <StyledSection>
-        {profileDetails && (
-          data.map((movie, index) => (
-            <StyledLink key={nanoid()} to={toMovie({ id: movie.id })}>
-              <MediumTile
-                src={
-                  !!movie.poster_path
-                    ? `${apiImage}/w200${movie.poster_path}?api_key=${apiKey}`
-                    : noPoster
-                }
-                title={movie.title}
-                subtitle={
-                  !!movie.release_date &&
-                  `
+  let component;
+  switch (type) {
+    case "movies":
+      component =
+        resultsPage.map((movie) => (
+          <StyledLink key={nanoid()} to={toMovie({ id: movie.id })}>
+            <MediumTile
+              key={movie.id}
+              src={
+                !!movie.poster_path
+                  ? `${apiImage}/w200${movie.poster_path}?api_key=${apiKey}`
+                  : noPoster
+              }
+              title={movie.title}
+              subtitle={!!movie.release_date ? getYearFromDate(movie.release_date) : ""}
+              tags={!!movie.genre_ids ? getGenreNames(movie.genre_ids, genres) : []}
+              rating={movie.vote_average}
+              votes={movie.vote_count}
+            />
+          </StyledLink>
+        ))
+      break;
+      
+    case "people":
+      component =
+        resultsPage.map((person) => (
+          <StyledLink key={nanoid()} to={toProfile({ id: person.id })}>
+            <SmallTile
+              key={person.id}
+              src={
+                !!person.profile_path
+                  ? `${apiImage}/w200${person.profile_path}?api_key=${apiKey}`
+                  : noProfile
+              }
+              title={person.name}
+            />
+          </StyledLink>
+        ))
+      break;
+
+    case "movie":
+      component =
+        data.map((person, index) => (
+          <StyledLink key={nanoid()} to={toProfile({ id: person.id })}>
+            <SmallTile
+              key={nanoid()}
+              src={
+                !!person.profile_path
+                  ? `${apiImage}/w500${person.profile_path}?api_key=${apiKey}`
+                  : noProfile
+              }
+              title={person.name}
+              subtitle={crew ? person.job : person.character}
+              hidden={index > hideIndex && !showMore}
+            />
+          </StyledLink>
+        ))
+      break;
+
+    case "profile":
+      component =
+        data.map((movie, index) => (
+          <StyledLink key={nanoid()} to={toMovie({ id: movie.id })}>
+            <MediumTile
+              src={
+                !!movie.poster_path
+                  ? `${apiImage}/w200${movie.poster_path}?api_key=${apiKey}`
+                  : noPoster
+              }
+              title={movie.title}
+              subtitle={
+                !!movie.release_date &&
+                `
                 ${crew ? movie.job : movie.character} 
                 ${movie.release_date ? "(" : ""}${getYearFromDate(movie.release_date)}${movie.release_date ? ")" : ""}
                 `
-                }
-                tags={!!movie.genre_ids && getGenreNames(movie.genre_ids, genres)}
-                rating={movie.vote_average}
-                votes={movie.vote_count}
-                hidden={index > hideIndex && !showMore}
-              />
-            </StyledLink>
-          )))}
-        {movieDetails && (
-          data.map((person, index) => (
-            <StyledLink key={nanoid()} to={toProfile({ id: person.id })}>
-              <SmallTile
-                key={nanoid()}
-                src={
-                  !!person.profile_path
-                    ? `${apiImage}/w500${person.profile_path}?api_key=${apiKey}`
-                    : noProfile
-                }
-                title={person.name}
-                subtitle={crew ? person.job : person.character}
-                hidden={index > hideIndex && !showMore}
-              />
-            </StyledLink>
-          )))}
+              }
+              tags={!!movie.genre_ids && getGenreNames(movie.genre_ids, genres)}
+              rating={movie.vote_average}
+              votes={movie.vote_count}
+              hidden={index > hideIndex && !showMore}
+            />
+          </StyledLink>
+        ))
+      break;
+  }
+  return (
+    <>
+      <StyledSection>
+        {component}
       </StyledSection>
       {(data.length > (hideIndex + 1)) && (
         <Button onClick={() => setShowMore(!showMore)}>
