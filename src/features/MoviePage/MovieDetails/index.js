f698d3b35df4e3c370d58f8ade7c0edc5f599332
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-21 22:28:26 +0200

    formatting

--- src/features/MoviePage/MovieDetails/index.js
+++ src/features/MoviePage/MovieDetails/index.js
@@ -1,58 +1,55 @@
 import { useSelector } from "react-redux";
 import { apiImage, apiKey } from "../../../common/commonValues";
-import { Header } from "../../../common/Container/index";
-import { selectItemData } from "../../itemSlice";
 import { Container } from "../../../common/Container/index";
-import noPoster from "../../../assets/noPoster.svg";
-import { getYearFromDate } from "../../getYearFromDate";
+import { Header } from "../../../common/Container/index";
 import { BigTile } from "../../../common/Tile/BigTile";
+import { selectItemData } from "../../itemSlice";
+import { getYearFromDate } from "../../getYearFromDate";
 import Cover from "../../../common/Cover";
 import Section from "../../../common/Section";
+import noPoster from "../../../assets/noPoster.svg";
 
 const MovieDetails = () => {
-    const movie = useSelector(selectItemData);
+  const movie = useSelector(selectItemData);
 
-    return (
-        <>
-            {movie.backdrop_path && (
-                <Cover
-                    src={`${apiImage}/original${movie.backdrop_path}?api_key=${apiKey}`}
-                    title={movie.title}
-                    rating={movie.vote_average}
-                    votes={movie.vote_count}
-                />
-            )}
-            <Container>
-                <BigTile
-                    src={
-                        !!movie.poster_path
-                            ? `${apiImage}/w500${movie.poster_path}?api_key=${apiKey}`
-                            : noPoster
-                    }
-                    title={movie.title}
-                    subtitle={!!movie.release_date ? getYearFromDate(movie.release_date) : ""}
-                    production={(!!movie.production_countries && (movie.production_countries.length > 0)) ? movie.production_countries[0].name : ""}
-                    releaseDate={!!movie.release_date ? movie.release_date.replaceAll(`-`, '.') : ""}
-                    tags={movie.genres}
-                    rating={movie.vote_average}
-                    votes={movie.vote_count}
-                    description={movie.overview}
-                />
-                <Header>Cast</Header>
-                <Section
-                    movieDetails={true}
-                />
-
-                <Header>Crew</Header>
-                <Section
-                    movieDetails={true}
-                    crew={true}
-                />
-
-
-            </Container>
-        </>
-    );
+  return (
+    <>
+      {movie.backdrop_path && (
+        <Cover
+          src={`${apiImage}/original${movie.backdrop_path}?api_key=${apiKey}`}
+          title={movie.title}
+          rating={movie.vote_average}
+          votes={movie.vote_count}
+        />
+      )}
+      <Container>
+        <BigTile
+          src={
+            !!movie.poster_path
+              ? `${apiImage}/w500${movie.poster_path}?api_key=${apiKey}`
+              : noPoster
+          }
+          title={movie.title}
+          subtitle={!!movie.release_date ? getYearFromDate(movie.release_date) : ""}
+          production={(!!movie.production_countries && (movie.production_countries.length > 0)) ? movie.production_countries[0].name : ""}
+          releaseDate={!!movie.release_date ? movie.release_date.replaceAll(`-`, '.') : ""}
+          tags={movie.genres}
+          rating={movie.vote_average}
+          votes={movie.vote_count}
+          description={movie.overview}
+        />
+        <Header>Cast</Header>
+        <Section
+          movieDetails={true}
+        />
+        <Header>Crew</Header>
+        <Section
+          movieDetails={true}
+          crew={true}
+        />
+      </Container>
+    </>
+  );
 };
 
 export default MovieDetails;
