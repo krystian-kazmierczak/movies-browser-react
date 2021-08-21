0ec3ca52f6edeacca45f0839c289689199c87451
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-21 22:23:48 +0200

    move Section from Container to separate folder

---
+++ src/common/Section/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledSection = styled.section`
+  margin-bottom: 16px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 12px;
+  width: 100%;
+  justify-content: center;
+  align-items: stretch;
+`;
