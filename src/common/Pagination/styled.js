a1aa29c5b42ea2a40044af4de5f102fc6e085648
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-08-21 22:22:12 +0200

    change PaginationButton to Button and move to separate folder (with Arrow and Label)

--- src/common/Pagination/styled.js
+++ src/common/Pagination/styled.js
@@ -1,63 +1,11 @@
 import styled from "styled-components";
+import { Arrow } from "../Button";
 
 export const Wrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
     margin: 40px 16px 16px 16px;
-`;
-
-export const PaginationButton = styled.button`
-    padding: 8px 16px;
-    margin-right: 12px;
-    background-color: ${({ theme }) => theme.colors.pattensBlue};
-    color: ${({ theme }) => theme.colors.woodsmoke};
-    border-radius: 5px;
-    font-size: 14px;
-    line-height: 20px;
-    border: none;
-    display: grid;
-    grid-auto-flow: column;
-    grid-gap: 8px;
-    align-items: center;
-    cursor: pointer;
-    transition: 0.3s;
-    
-    &:disabled {
-        background-color: ${({ theme }) => theme.colors.cornsilk};
-        color: ${({ theme }) => theme.colors.woodsmoke};
-        cursor: not-allowed;
-    }
-
-    &:hover {
-        filter: brightness(80%);
-    }
-
-    &:active {
-        filter: brightness(90%);
-    }
-
-    @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
-        grid-gap: 4px;
-        padding: 8px 12px;
-        margin-right: 8px;
-    }
-`;
-
-export const Label = styled.span`
-    @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
-        display: none;
-    }
-`;
-
-export const Arrow = styled.img`
-    width: 7px;
-    height: 11px;
-
-    @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
-        width: 5px;
-        height: 8px;
-    }
 `;
 
 export const SecondArrow = styled(Arrow)`
