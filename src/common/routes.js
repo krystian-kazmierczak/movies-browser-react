d3ed9b7a1992c156b610dd5649f0643f096818ff
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-08-24 11:16:23 +0200

    correct path movies->movie, people->person

--- src/common/routes.js
+++ src/common/routes.js
@@ -2,5 +2,5 @@ export const toMovies = () => "/movies";
 export const toPeople = () => "/people";
 export const toError = () => "/error";
 
-export const toMovie = ({ id } = { id: ":id" }) => `/movies/${id}`;
-export const toProfile = ({ id } = { id: ":id" }) => `/people/${id}`;
+export const toMovie = ({ id } = { id: ":id" }) => `/movie/${id}`;
+export const toProfile = ({ id } = { id: ":id" }) => `/person/${id}`;
